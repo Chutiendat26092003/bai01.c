b4d1472554347bfae040ec65f6fc6f43b82a0d7e
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-29 13:50:45 +0700

    Add files via upload

---
+++ bai01.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("Class: T2109M\n");
+	printf("Ro1No: ?\n");
+	printf("Full Name: Chu Tien Dat\n");
+	printf("Email: DatCTTH2109014@fpt.edu.vn\n");
+	printf("Phone: 0392929807\n");
+	
+	
+	return 0;
+}
